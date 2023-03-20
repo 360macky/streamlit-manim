66a3af1a9335fa386294ae961832130e8541acb7
Marcelo Arias <hello@marceloarias.com>
2023-03-19 19:52:01 -0500

    Update

--- src/main.py
+++ src/main.py
@@ -1,14 +1,12 @@
 import streamlit as st
-from manim.mobject.geometry import Circle
-from manim.animation.creation import Create
-from manim.scene.scene import Scene
+from manim import *
 from manim.utils.color import Colors
 
 st.title("Manim")
 st.write("This is a test of Manim in Streamlit")
 
 prompt = st.text_area("Write your animation idea here")
-color = st.text_input("Give Hex Fill color for circle", value=Colors.blue_e.value)
+color = st.text_input("Give Hex Fill color for circle", value="#FF0000")
 opacity = st.slider("Choose opacity", min_value=0.0, max_value=1.0, value=0.45)
 
 class SquareToCircle(Scene):
@@ -20,4 +18,5 @@ class SquareToCircle(Scene):
 if st.button("Render"):
     SquareToCircle().render()
     st.video("media/videos/1080p60.0/SquareToCircle.mp4")
+
 
