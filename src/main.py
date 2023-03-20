8c9b1f010a43226d14f9727d6bb409182c57fd9c
Marcelo Arias <hello@marceloarias.com>
2023-03-19 20:29:29 -0500

    Update

--- src/main.py
+++ src/main.py
@@ -4,20 +4,22 @@ from manim import *
 st.title("Manim")
 st.write("This is a test of Manim in Streamlit")
 
-prompt = st.text_area("Write your animation idea here")
-opacity = st.slider("Choose opacity", min_value=0.0, max_value=1.0, value=0.45)
-
 code_snippet = '''
 circle = Circle()
 circle.set_fill("#FF0000", opacity=opacity)
 self.play(Create(circle))
 '''
 
+prompt = st.text_area("Write your animation idea here")
+code_input = st.text_area("Write your animation idea here", value=code_snippet)
+opacity = st.slider("Choose opacity", min_value=0.0, max_value=1.0, value=0.45)
+
+
 st.code(code_snippet, language="python")
 
 class GeneratedScene(Scene):
     def construct(self):
-        exec(code_snippet)
+        exec(code_input)
 
 
 if st.button("Render"):
