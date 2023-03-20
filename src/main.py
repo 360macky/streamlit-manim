e5a8f145b95409a0065fec69147c77e7a218a328
Marcelo Arias <hello@marceloarias.com>
2023-03-19 20:12:58 -0500

    Update

--- src/main.py
+++ src/main.py
@@ -8,9 +8,9 @@ prompt = st.text_area("Write your animation idea here")
 opacity = st.slider("Choose opacity", min_value=0.0, max_value=1.0, value=0.45)
 
 code_snippet = '''
-circle = Circle()                   
-circle.set_fill(color, opacity=opacity)
-self.play(Create(circle))
+        circle = Circle()
+        circle.set_fill("#FF0000", opacity=opacity)
+        self.play(Create(circle))
 '''
 
 st.code(code_snippet, language="python")
@@ -23,5 +23,6 @@ class GeneratedScene(Scene):
 
 
 if st.button("Render"):
-    SquareToCircle().render()
+    GeneratedScene().render()
     st.video("media/videos/1080p60.0/GeneratedScene.mp4")
+
