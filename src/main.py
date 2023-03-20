f3b9eaa5500e2f8eac30a4b453cf057e948d4446
Marcelo Arias <hello@marceloarias.com>
2023-03-19 23:11:06 -0500

    Update

--- src/main.py
+++ src/main.py
@@ -12,15 +12,13 @@ self.play(Create(circle))
 '''
 
 prompt = st.text_area("Write your animation idea here")
-code_input = st.text_area("Write your animation idea here", value=code_snippet)
 openai_key = st.text_input("Write your OpenAI API Key", value="", type="password")
+code_input = st.text_area("Write your animation idea here", value=code_snippet)
 
-class GeneratedScene(Scene):
-    def construct(self):
-        exec(code_input)
-
-
-if st.button("Render", type="primary"):
+if st.button("Generate animation", type="primary"):
+    class GeneratedScene(Scene):
+        def construct(self):
+            exec(code_input)
     GeneratedScene().render()
     st.video("media/videos/1080p60.0/GeneratedScene.mp4")
 
