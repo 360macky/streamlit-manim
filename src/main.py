816f83ff88c24feb4654b337acb3141477d99f7d
Marcelo Arias <hello@marceloarias.com>
2023-03-19 20:01:21 -0500

    Update

--- src/main.py
+++ src/main.py
@@ -8,14 +8,21 @@ prompt = st.text_area("Write your animation idea here")
 color = st.text_input("Give Hex Fill color for circle", value="#FF0000")
 opacity = st.slider("Choose opacity", min_value=0.0, max_value=1.0, value=0.45)
 
+code_snippet = '''
+    circle = Circle()                   
+    circle.set_fill(color, opacity=opacity)
+    self.play(Create(circle))
+'''
+
+st.code(code_snippet, language="python")
+
 class SquareToCircle(Scene):
     def construct(self):
-        circle = Circle()                   
+        circle = Circle()
         circle.set_fill(color, opacity=opacity)
         self.play(Create(circle))
+
 
 if st.button("Render"):
     SquareToCircle().render()
     st.video("media/videos/1080p60.0/SquareToCircle.mp4")
-
-
