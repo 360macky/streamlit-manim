9d88def62fdfe131f8729e3b1794d6bb8290f12b
Marcelo Arias <hello@marceloarias.com>
2023-03-19 23:19:40 -0500

    update system openai

--- src/main.py
+++ src/main.py
@@ -12,13 +12,24 @@ self.play(Create(circle))
 '''
 
 prompt = st.text_area("Write your animation idea here")
-openai_key = st.text_input("Write your OpenAI API Key", value="", type="password")
+openai.api_key = st.text_input(
+    "Write your OpenAI API Key", value="", type="password")
 code_input = st.text_area("Write your animation idea here", value=code_snippet)
 
 if st.button("Generate animation", type="primary"):
+
+    response = openai.Completion.create(
+        engine="gpt-3.5-turbo",
+        prompt=[{"role": "system", "content": "You will write Manim script for animations in Python. Generate code, not text. Do not use any other library. Do not explain. At the end use 'self.play'."},
+                {"role": "user", "content": f"Request Manim animation of: {prompt}."}],
+    )
+
+    code_response = response["choices"][0]["text"]
+
+    logger.info(f"Code response: {code_response}")
+
     class GeneratedScene(Scene):
         def construct(self):
             exec(code_input)
     GeneratedScene().render()
     st.video("media/videos/1080p60.0/GeneratedScene.mp4")
-
